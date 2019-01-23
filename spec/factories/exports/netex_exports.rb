ce4c79b7f3c932a035149e00a5b568bc7604382d
Zog <johan@about-blank.fr>
2019-01-23 15:30:53 +0100

    Refs #7829; Fix specs

--- spec/factories/exports/netex_exports.rb
+++ spec/factories/exports/netex_exports.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :netex_export, class: Export::Netex, parent: :export do
+    association :referential, factory: :workbench_referential
     association :parent, factory: :workgroup_export
     options({export_type: :line, duration: 90})
     type 'Export::Netex'
