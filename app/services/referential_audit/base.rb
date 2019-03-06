fe3a8b48acd0b5ee6e90427fbdfb528dd3d4c9cc
Zog <johan@about-blank.fr>
2019-03-06 10:15:02 +0100

    Refs #10491; Add audit on VehicleJourneyAtStops
    Check that they all have at least on time set

--- app/services/referential_audit/base.rb
+++ app/services/referential_audit/base.rb
@@ -39,7 +39,8 @@ class ReferentialAudit
   end
 end
 
-require_dependency 'referential_audit/vehicle_journey_initial_offset'
+require_dependency 'referential_audit/checksums'
 require_dependency 'referential_audit/journey_pattern_distances'
 require_dependency 'referential_audit/journey_pattern_stop_points'
-require_dependency 'referential_audit/checksums'
+require_dependency 'referential_audit/vehicle_journey_at_stop_times'
+require_dependency 'referential_audit/vehicle_journey_initial_offset'
