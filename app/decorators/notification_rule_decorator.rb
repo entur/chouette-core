2d3930b5704e467f2f51e4839fb62009860c62cd
Aurelien-af83 <aurelien.guiramand@gmail.com>
2019-04-16 17:55:33 +0200

    Small notification_rules label fixes

--- app/decorators/notification_rule_decorator.rb
+++ app/decorators/notification_rule_decorator.rb
@@ -2,8 +2,10 @@ class NotificationRuleDecorator < AF83::Decorator
   decorates NotificationRule
   set_scope { context[:workbench] }
 
-  create_action_link
-  
+  create_action_link do |l|
+    l.content t('notification_rules.actions.new')
+  end
+
   with_instance_decorator do |instance_decorator|
 
     instance_decorator.show_action_link
