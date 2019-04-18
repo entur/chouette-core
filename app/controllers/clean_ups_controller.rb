4699249cd83d432f7705d1c6de2331ad8b118500
Zog <johan@about-blank.fr>
2019-04-18 17:28:45 +0200

    Refs #11504; Set default dates for cleanup

--- app/controllers/clean_ups_controller.rb
+++ app/controllers/clean_ups_controller.rb
@@ -13,6 +13,12 @@ class CleanUpsController < ChouetteController
     end
   end
 
+  def new
+    build_resource
+    @clean_up.begin_date = @referential.metadatas_period.min
+    @clean_up.end_date = @referential.metadatas_period.max
+  end
+
   def get_data_cleanups
     data_cleanups_params.keys.select do |k|
       data_cleanups_params[k] == 'true'
@@ -24,7 +30,18 @@ class CleanUpsController < ChouetteController
   end
 
   def clean_up_params
-    params.require(:clean_up).permit(:date_type, :begin_date, :end_date).update(data_cleanups: get_data_cleanups)
+    clean_up_params = params.require(:clean_up).permit(:date_type, :begin_date, :end_date).update(data_cleanups: get_data_cleanups)
+    if clean_up_params[:date_type] == 'outside'
+      clean_up_params.delete 'begin_date(3i)'
+      clean_up_params.delete 'begin_date(2i)'
+      clean_up_params.delete 'begin_date(1i)'
+      clean_up_params.delete 'end_date(3i)'
+      clean_up_params.delete 'end_date(2i)'
+      clean_up_params.delete 'end_date(1i)'
+      clean_up_params[:begin_date] = @referential.metadatas_period.min
+      clean_up_params[:end_date] = @referential.metadatas_period.max
+    end
+    pp clean_up_params
   end
 
   def begin_of_association_chain
