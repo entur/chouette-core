9a832c3576a304a20bbf878f9c3efabe4a620b9b
Johan Van Ryseghem <johan@about-blank.fr>
2019-05-10 15:55:18 +0200

    Refs #11701; Fix libs loading

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,7 @@ require File.expand_path('../boot', __FILE__)
 ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
 
 require 'rails/all'
-require_relative '../lib/smart_env'
-require_relative '../lib/version'
+require_relative '../app/lib/smart_env'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,7 +19,6 @@ module ChouetteIhm
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << config.root.join('lib')
     config.autoload_paths << config.root.join('app', 'jobs')
 
     # custom exception pages
